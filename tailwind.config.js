aa72312489b878200aa6f5c11f277b381e4664d6
Muhammad Kaif Nazeer <muhammadkaifnazeer99@gmail.com>
2024-06-14 18:47:13 +0500

    uploaded on github

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#F97316',
+      },
+      fontFamily: {
+        'display': ['Poppins', 'sans-serif'],
+        'body': ['Inter', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
